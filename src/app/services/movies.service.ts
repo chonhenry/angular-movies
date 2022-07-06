0669c48b99f573635552824efc0dfa56d54007ff
Henry Chon 2 <chonhenry@outlook.com>
2022-07-06 07:29:18 -0400

    commit

---
+++ src/app/services/movies.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MoviesService {
+  constructor(private http: HttpClient) {}
+
+  getMovies() {
+    return this.http.get(
+      'https://api.themoviedb.org/3/movie/popular?api_key=c3cea5dfe524b09cb4548284a077e8f0'
+    );
+  }
+}
